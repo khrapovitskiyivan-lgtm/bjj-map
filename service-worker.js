9ff439144d7fce8ff388b753b1bd66d5c2f467e9
khrapovitskiyivan-lgtm <khrapovitskiyivan@gmail.com>
2026-06-08 16:51:55 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,20 @@
-const CACHE_NAME = 'bjj-map-v2';
-const urlsToCache = ['/', '/index.html', '/styles.css', '/main.js', '/manifest.json'];
+const CACHE_NAME = 'bjj-map-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+            })
+  );
 });
